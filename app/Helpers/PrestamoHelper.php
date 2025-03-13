466d8ba809f7ffd3d5a6bf0d8b65e939358f35a7
asdf <alexrayo.net@gmail.com>
2025-03-13 13:33:39 -0600

    [update]actualizar fecha_ultimo_pago del prestamo cuando se ANULA/ACTIVA un abono

--- app/Helpers/PrestamoHelper.php
+++ app/Helpers/PrestamoHelper.php
@@ -67,6 +67,7 @@ class PrestamoHelper
   {
     $prestamo = Prestamo::find($abono->prestamo_id);
 
+    //Actualizar estado de la caja relacionada
     $caja = Caja::where('referencia', $abono->id)->first();
     if ($caja) {
       $caja->estado = $abono->estado;
@@ -77,8 +78,24 @@ class PrestamoHelper
     //si el abono es anulado, se revierte el saldo
     if ($prestamo && $abono->estado === "ANULADO") {
       $prestamo->saldo += $abono->abono_capital;
+
+      // Buscar último abono ACTIVO (excluyendo el actual)
+      $ultimoAbonoActivo = Abono::where('prestamo_id', $prestamo->id)
+        ->where('estado', 'ACTIVO')
+        ->where('id', '!=', $abono->id)
+        ->latest('fecha_pago')
+        ->first();
+      //Actualizamos la fecha_ultimo_pago tomando la fecha_pago
+      //si no se encuentran ningún abono se asigna la fecha_desembolso del préstamo
+      $prestamo->fecha_ultimo_pago = $ultimoAbonoActivo
+        ? $ultimoAbonoActivo->fecha_pago
+        : $prestamo->fecha_desembolso;
+
       $prestamo->save();
     } elseif ($prestamo && $abono->estado === "ACTIVO") {
+      //Nos aseguramos de tener la última fecha actualizada cuando el estado es "ACTIVO"
+      //y actualizamod el saldo
+      $prestamo->fecha_ultimo_pago = $abono->fecha_pago;
       $prestamo->saldo -= $abono->abono_capital;
       $prestamo->save();
     } else {
